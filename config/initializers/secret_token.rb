702542c22bf1201aa5145939fd2b03566f934727
Igor Buryan <toscatland@gmail.com>
2013-11-30 15:23:47 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FastTrip::Application.config.secret_token = '23548698a3dc093480b4376cf41ac59e06a3feee1607ce28056fe17e7bdb19e2173c01fb31a2a3374a50f742dbed8b3a809e0573bf75b9ab3153d6d6d185b577'
