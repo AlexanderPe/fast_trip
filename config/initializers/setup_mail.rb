b3267d1cfe92f083a3a517dc98973f10f9792329
Igor Buryan <toscatland@gmail.com>
2013-12-15 05:23:10 +0200

    first css

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {  
-      :address              => "smtp.gmail.com",  
-      :port                 => 587,  
-      :domain               => "gmail.com",  
-      :user_name            => "i.n.buryan@gmail.com",  
-      :password             => "gfvznbjktuf89",  
-      :authentication       => "plain",  
-      :enable_starttls_auto => true  
-   }  
